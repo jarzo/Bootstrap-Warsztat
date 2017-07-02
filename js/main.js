5c9fade8159a06b878984238380c75f09e3df9df
Michał Jarząbek <nebraska994@gmail.com>
2017-07-02 16:40:59 +0200

    poprawione port folio żeby nie nachodziło

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 'use strict';
 
 
+
 $(function () {
+
+	$("a[href*='#']").click(function(){
+		$("body").animate({
+			scrollTop: $(this.hash).offset().top - 50
+		}, 500);
+	});
+
 	$(window).scroll(function () {
 		if ($(window).scrollTop() >= 50) {
 			$("#main-nav").addClass("scroll");
-			$("#main-nav").css({"transition":"all 0.3s"});
+			$("#main-nav").css({
+				"transition": "all 0.3s"
+			});
 		} else {
 			$("#main-nav").removeClass("scroll");
 		}
 	});
 	if ($(window).scrollTop() >= 50) {
-			$("#main-nav").addClass("scroll");
+		$("#main-nav").addClass("scroll");
 	}
 });
+//zmana koloru menu po zjeżdżaniu
